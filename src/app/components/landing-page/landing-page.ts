8b65680ee28125c552a155ef549b7bd81a5e2e41
Lucas Ferreira Souza <74795990+lucaszfzs@users.noreply.github.com>
2025-11-25 22:54:37 -0300

    update landing page
    Conclui a landing page

--- src/app/components/landing-page/landing-page.ts
+++ src/app/components/landing-page/landing-page.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { HeroSection } from "./hero-section/hero-section";
 import { FeaturesSection } from "./features-section/features-section";
+import { PricingSection } from "./pricing-section/pricing-section";
+import { TestimonialsSection } from "./testimonials-section/testimonials-section";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Header, HeroSection, FeaturesSection],
+  imports: [Header, HeroSection, FeaturesSection, PricingSection, TestimonialsSection, Footer],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
