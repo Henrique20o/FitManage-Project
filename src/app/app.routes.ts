cac6893e89e4d75c5d05f43e0a627ea72ab4e059
Lucas Ferreira Souza <74795990+lucaszfzs@users.noreply.github.com>
2025-11-25 23:23:47 -0300

    update login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { LandingPage } from './components/landing-page/landing-page';
+import { Login } from './components/login/login';
+import { Cadastro } from './components/cadastro/cadastro';
 
 export const routes: Routes = [
     {
         path: "",
         component: LandingPage
+    },
+    {
+        path: "login",
+        component: Login
+    },
+    {
+        path: "cadastro",
+        component: Cadastro
     }
 ];
