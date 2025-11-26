f8c5dc77474e36e89f701a5d8fb1973e2de187ff
Lucas Ferreira Souza <74795990+lucaszfzs@users.noreply.github.com>
2025-11-26 12:05:04 -0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,13 @@ import { LandingPage } from './components/landing-page/landing-page';
 import { Login } from './components/login/login';
 import { Cadastro } from './components/cadastro/cadastro';
 import { Dashboard } from './components/dashboard/dashboard';
+import { Alunos } from './components/alunos/alunos';
 
 export const routes: Routes = [
-    {
-        path: "",
-        component: LandingPage
-    },
-    {
-        path: "login",
-        component: Login
-    },
-    {
-        path: "cadastro",
-        component: Cadastro
-    }
+    {path: "", component: LandingPage},
+    {path: "login", component: Login},
+    {path: "cadastro", component: Cadastro},
+    {path: "dashboard-inicio", component: Dashboard},
+    {path: "alunos" , component: Alunos}
 ];
  
